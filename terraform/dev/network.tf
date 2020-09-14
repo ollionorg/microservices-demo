40cfb2bf9a85338ef5a37fffb4e9d480abd60557
nadzir <nadzir@cldcvr.com>
2020-09-14 15:21:08 +0800

    add flow logs

--- terraform/dev/network.tf
+++ terraform/dev/network.tf
@@ -15,6 +15,12 @@ resource "google_compute_subnetwork" "gke" {
   ip_cidr_range            = var.gke_subnet_cidr_range
   private_ip_google_access = true
 
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+
   secondary_ip_range {
     range_name    = "secondary-range-pods"
     ip_cidr_range = var.gke_subnet_cidr_range_pod
